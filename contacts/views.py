3799df53c72f50332552b77f110dc1d69ba92c8a
peggylsy <peggyleung.sy@yahoo.com>
2026-01-08 14:06:37 +0800

    step 15:Tag & send email

--- contacts/views.py
+++ contacts/views.py
@@ -2,32 +2,43 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Contact
 from django.contrib import messages
 from .forms import ContactForm
+from django.core.mail import send_mail
 # Create your views here.
 def contact(request):
-    if request.method=='POST':
-        listing = request.POST[ 'listing' ]
-        listing_id = request.POST[ 'listing_id' ]
-        name = request.POST[ 'name' ]
-        email = request.POST[ 'email' ]
-        phone=request.POST[ 'phone' ]
-        message = request.POST[ 'message' ]
-        user_id = request.POST[ 'user_id' ]
-        if request.user.is_authenticated:
-            has_contacted = Contact.objects.all( ).filter(listing_id=listing_id, user_id=user_id)
-            if  has_contacted:
-                messages.error(request, 'You have already made an inquiry for this listing')
-                return redirect ('listings:listing', listing_id=listing_id)
-        contact = Contact(listing=listing, listing_id=listing_id, name=name, 
-                          email=email, phone=phone, message=message, user_id=user_id)
-        contact.save( )
-        messages.success(request, 'Your request has been submitted, a clinic representative ' \
-        'will get back to you soon.')
-        return redirect ('listings:listing', listing_id=listing_id)
+        if request.method=='POST':
+            listing = request.POST[ 'listing' ]
+            listing_id = request.POST[ 'listing_id' ]
+            name = request.POST[ 'name' ]
+            email = request.POST[ 'email' ]
+            phone=request.POST[ 'phone' ]
+            message = request.POST[ 'message' ]
+            user_id = request.POST[ 'user_id' ]
+            doctor_email = request.POST[ 'doctor_email' ]
+            if request.user.is_authenticated:
+                has_contacted = Contact.objects.all( ).filter(listing_id=listing_id, user_id=user_id)
+                if  has_contacted:
+                    messages.error(request, 'You have already made an inquiry for this listing')
+                    return redirect ('listings:listing', listing_id=listing_id)
+            contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                                                email=email, phone=phone, message=message, user_id=user_id)
+            contact.save( )
+            # ======== sen d mail''
+            send_mail(
+                    'Clinic Inquiry',
+                    'There has been an inquiry for ' + listing +'. Sign into the admin panel for more info',
+                    'yuv29458@gmail.com',
+                    [doctor_email],
+                    fail_silently=False,
+            )
+            # ==========
+            messages.success(request, 'Your request has been submitted, a clinic representative will get back to you soon.')
+            return redirect ('listings:listing', listing_id=listing_id)
+    
 def delete_contact(request, contact_id):
         contact = get_object_or_404(Contact,pk=contact_id)
         contact.delete( )
     # Logic to delete the contact with the given contact_id
-        return redirect( 'accounts:dashboard')
+        return redirect(  'accounts:dashboard' )
 
 def edit_contact(request, contact_id):
         contact = get_object_or_404(Contact, pk=contact_id)
@@ -39,4 +50,4 @@ def edit_contact(request, contact_id):
         else:
             form = ContactForm(instance=contact)
     # Logic to edit the contact with the given contact_id
-        return render(request, "contacts/edit_contact.html", {"form": form, "contact": contact})
+        return render(request, "contacts/edit_contact.html", {"form":  form, "contact":  contact})
--- listings/admin.py
+++ listings/admin.py
@@ -1,12 +1,46 @@
 from django.contrib import admin
-from .models import Listing
-
+from django import forms
+from .models import Listing, Subject
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from taggit.forms import TagWidget
+from django.forms import NumberInput
+from django.db import models
 # Register your models here.
+
+class ListingAdminForm(forms.ModelForm):
+    professions = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(),
+        widget=FilteredSelectMultiple(verbose_name="Professions", is_stacked=False,
+                attrs={'rows' : '5'}), required=False, label="Select Professions")
+    class Meta:
+        model = Listing
+        fields = '__all__'
+        widget = {"services" : TagWidget()}
+
 class ListingAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'district', 'is_published', 'rooms', 'doctor',
-    list_display_links = 'id', 'title'
-    list_editable = 'is_published', 'rooms',
-    search_fields = 'title', 'district', 'doctor_name'
-    list_per_page = 25
+        list_display = 'id', 'title', 'district', 'is_published', 'rooms', 'doctor', 'tag_list', 'display_professions'
+        list_display_links = 'id', 'title'
+        list_filter = ("doctor", "services")
+        list_editable = 'is_published', 'rooms',
+        search_fields = 'title', 'district', 'doctor__name', 'services__name', 'professions__name'
+        list_per_page = 25
+        formfield_overrides = {
+        models.IntegerField: {
+        "widget" : NumberInput(attrs = {"size" : "5"})
+        }
+}
+        #show filter#
+        show_facets  =  admin.ShowFacets.ALWAYS
+        def get_queryset(self, request):
+            return super().get_queryset(request).prefetch_related("services", "professions")
+        
+        def display_professions(self,obj):
+            return ", ".join([subject.name for subject in obj.professions.all()]) or "None"
+        display_professions.short_description = "Professions"
+        
+class SubjectAdmin(admin.ModelAdmin):
+        list_display = "name",
+        search_fields = "name",
 
 admin.site.register(Listing, ListingAdmin)
+admin.site.register(Subject, SubjectAdmin)
